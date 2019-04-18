e6240b7e3db164f935cf7fb006e6e224818e73eb
pranjal Jain <prnjljn@gmail.com>
2019-04-18 16:32:45 +0530

    Stable Position

--- app/src/main/java/com/example/floorplan/MainActivity.java
+++ app/src/main/java/com/example/floorplan/MainActivity.java
@@ -6,9 +6,12 @@ import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,21 +22,15 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
-import org.opencv.core.DMatch;
-import org.opencv.core.KeyPoint;
+
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfDMatch;
-import org.opencv.core.MatOfKeyPoint;
+
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
-import org.opencv.features2d.DescriptorExtractor;
-import org.opencv.features2d.DescriptorMatcher;
-import org.opencv.features2d.FeatureDetector;
-import org.opencv.features2d.ORB;
 import org.opencv.imgproc.Imgproc;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -55,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
     private  MenuItem displayElement;
     private  MenuItem segmentRoom;
     private  MenuItem reset;
+    int x;
+    int y;
     private int threshold = 100;
 
 
@@ -119,6 +118,7 @@ public class MainActivity extends AppCompatActivity {
                     imgView.setImageBitmap(imageBitmap);
                     Button b = findViewById(R.id.button);
                     b.setVisibility(View.INVISIBLE);
+                    addOnTouchListener();
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                     Toast.makeText(this, "Unable to open imageBitmap", Toast.LENGTH_SHORT);
@@ -162,60 +162,89 @@ public class MainActivity extends AppCompatActivity {
         imgView.setImageBitmap(tempImageBitmap);
 
     }
-    public int getMinimumIndex(List<Double> s){
-        int minIndex=0;
-        for(int i=0;i<s.size();i++){
-            if(s.get(minIndex)>s.get(i)){
-                minIndex=i;
+    public void addOnTouchListener(){
+        ImageView temp =imgView;
+        temp.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                x =(int)event.getX();
+                y = (int)event.getY();
+                    String text = "You click at x = " + x + " and y = " + y;
+                    Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                    displayElements();
+                return false;
             }
-
-        }
-        return minIndex;
+        });
     }
 
-
-
     public void displayElements() {
-        Utils.bitmapToMat(imageBitmap,imageMat);
+        Utils.bitmapToMat(imageBitmap, imageMat);
         Mat negImageMat = imageMat;
         Imgproc.threshold(negImageMat, negImageMat, 100, 255, Imgproc.THRESH_BINARY);
         Core.bitwise_not(negImageMat, negImageMat);
-        Mat wall= getWall();
-        Core.bitwise_xor(negImageMat,wall,wall);
-        Core.bitwise_not(wall,wall);
+        Mat wall = getWall();
+        Core.bitwise_xor(negImageMat, wall, wall);
+        Core.bitwise_not(wall, wall);
 
 
-        List<Mat> object =new ArrayList<>();
+        List<Mat> object = new ArrayList<>();
         try {
-            object.add(0,Utils.loadResource(this, R.drawable.arm));
-            object.add(1,Utils.loadResource(this, R.drawable.bed));
-            object.add(2,Utils.loadResource(this, R.drawable.cofee));
-            object.add(3,Utils.loadResource(this, R.drawable.rtable));
-            object.add(4,Utils.loadResource(this, R.drawable.lsofa));
-            object.add(5,Utils.loadResource(this, R.drawable.ssofa));
-            object.add(6,Utils.loadResource(this, R.drawable.sink));
-            object.add(7,Utils.loadResource(this, R.drawable.twinsink));
-            object.add(8,Utils.loadResource(this, R.drawable.ssink));
-            object.add(9,Utils.loadResource(this, R.drawable.lsink));
-            object.add(10,Utils.loadResource(this, R.drawable.tub));
-            object.add(11,Utils.loadResource(this, R.drawable.dtable));
+            object.add(0, Utils.loadResource(this, R.drawable.bed));
+            object.add(1, Utils.loadResource(this, R.drawable.bed1));
+            object.add(2, Utils.loadResource(this, R.drawable.bed2));
+            object.add(3, Utils.loadResource(this, R.drawable.bed3));
+            object.add(4, Utils.loadResource(this, R.drawable.chair));
+            object.add(5, Utils.loadResource(this, R.drawable.chair1));
+            object.add(6, Utils.loadResource(this, R.drawable.chair2));
+            object.add(7, Utils.loadResource(this, R.drawable.chair3));
+            object.add(8, Utils.loadResource(this, R.drawable.ctable));
+            object.add(9, Utils.loadResource(this, R.drawable.dtable));
+            object.add(10, Utils.loadResource(this, R.drawable.dtable1));
+            object.add(11, Utils.loadResource(this, R.drawable.lsink));
+            object.add(12, Utils.loadResource(this, R.drawable.lsink1));
+            object.add(13, Utils.loadResource(this, R.drawable.lsink2));
+            object.add(14, Utils.loadResource(this, R.drawable.lsink3));
+            object.add(15, Utils.loadResource(this, R.drawable.lsofa));
+            object.add(16, Utils.loadResource(this, R.drawable.lsofa1));
+            object.add(17, Utils.loadResource(this, R.drawable.lsofa2));
+            object.add(18, Utils.loadResource(this, R.drawable.lsofa3));
+            object.add(19, Utils.loadResource(this, R.drawable.sink));
+            object.add(20, Utils.loadResource(this, R.drawable.sink1));
+            object.add(21, Utils.loadResource(this, R.drawable.sink2));
+            object.add(22, Utils.loadResource(this, R.drawable.sink3));
+            object.add(23, Utils.loadResource(this, R.drawable.ssink));
+            object.add(24, Utils.loadResource(this, R.drawable.ssink1));
+            object.add(25, Utils.loadResource(this, R.drawable.ssink2));
+            object.add(26, Utils.loadResource(this, R.drawable.ssink3));
+            object.add(27, Utils.loadResource(this, R.drawable.ssofa));
+            object.add(28, Utils.loadResource(this, R.drawable.ssofa1));
+            object.add(29, Utils.loadResource(this, R.drawable.ssofa2));
+            object.add(30, Utils.loadResource(this, R.drawable.ssofa3));
+            object.add(31, Utils.loadResource(this, R.drawable.tsink));
+            object.add(32, Utils.loadResource(this, R.drawable.tsink1));
+            object.add(33, Utils.loadResource(this, R.drawable.tsink2));
+            object.add(34, Utils.loadResource(this, R.drawable.tsink3));
+            object.add(35, Utils.loadResource(this, R.drawable.tub));
+            object.add(36, Utils.loadResource(this, R.drawable.tub1));
+            object.add(37, Utils.loadResource(this, R.drawable.tub2));
+            object.add(38, Utils.loadResource(this, R.drawable.tub3));
+            //object.add(39,Utils.loadResource(this,R.drawable.rtable));
 
 
-        }
-        catch (IOException e){
-            Toast t =Toast.makeText(this,"Unable to load object imsges",Toast.LENGTH_SHORT);
+        } catch (IOException e) {
+            Toast t = Toast.makeText(this, "Unable to load object imsges", Toast.LENGTH_SHORT);
             t.show();
         }
 
 
-        Mat cannyoutput =new Mat();
-        Imgproc.Canny(wall,cannyoutput,threshold,threshold*2);
-        List<MatOfPoint> contours =new ArrayList<>();
+        Mat cannyoutput = new Mat();
+        Imgproc.Canny(wall, cannyoutput, threshold, threshold * 2);
+        List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
-        Imgproc.findContours(cannyoutput,contours,hierarchy,Imgproc.RETR_TREE,Imgproc.CHAIN_APPROX_SIMPLE);
-        MatOfPoint2f[] contoursPoly  = new MatOfPoint2f[contours.size()];
+        Imgproc.findContours(cannyoutput, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        MatOfPoint2f[] contoursPoly = new MatOfPoint2f[contours.size()];
         Rect[] boundRect = new Rect[contours.size()];
-        for(int i=0;i<contours.size();i++){
+        for (int i = 0; i < contours.size(); i++) {
             contoursPoly[i] = new MatOfPoint2f();
             Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly[i], 3, true);
             boundRect[i] = Imgproc.boundingRect(new MatOfPoint(contoursPoly[i].toArray()));
@@ -227,73 +256,81 @@ public class MainActivity extends AppCompatActivity {
         }
         for (int i = 0; i < contours.size(); i++) {
             Scalar color = new Scalar(255);
-            Imgproc.drawContours(drawing, contoursPolyList, i, color,-1);
-            Imgproc.rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, -1);
+           // Imgproc.drawContours(drawing, contoursPolyList, i, color, -1);
+            if(boundRect[i].area()>800) {
+                Imgproc.rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, -1);
+            }
 
         }
-       Mat canny =new Mat();
-        Imgproc.Canny(drawing,canny,threshold,threshold*2);
-        List<MatOfPoint> countoursFinal=new ArrayList<>();
-        Mat hierarchyFinal =new Mat();
-        Imgproc.findContours(canny,countoursFinal,hierarchyFinal,Imgproc.RETR_TREE,Imgproc.CHAIN_APPROX_SIMPLE);
-        MatOfPoint2f[] countoursPolyFinal=new MatOfPoint2f[countoursFinal.size()];
+        Mat canny = new Mat();
+        Imgproc.Canny(drawing, canny, threshold, threshold * 2);
+        List<MatOfPoint> countoursFinal = new ArrayList<>();
+        Mat hierarchyFinal = new Mat();
+        Imgproc.findContours(canny, countoursFinal, hierarchyFinal, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        MatOfPoint2f[] countoursPolyFinal = new MatOfPoint2f[countoursFinal.size()];
         Rect[] boundRectFinal = new Rect[countoursFinal.size()];
-        for(int i=0;i<countoursFinal.size();i++){
-            countoursPolyFinal[i]=new MatOfPoint2f();
-            Imgproc.approxPolyDP(new MatOfPoint2f(countoursFinal.get(i).toArray()),countoursPolyFinal[i],3,true);
-            boundRectFinal[i]=Imgproc.boundingRect(new MatOfPoint(countoursPolyFinal[i].toArray()));
+        for (int i = 0; i < countoursFinal.size(); i++) {
+            countoursPolyFinal[i] = new MatOfPoint2f();
+            Imgproc.approxPolyDP(new MatOfPoint2f(countoursFinal.get(i).toArray()), countoursPolyFinal[i], 3, true);
+            boundRectFinal[i] = Imgproc.boundingRect(new MatOfPoint(countoursPolyFinal[i].toArray()));
         }
         Mat finalIm = Mat.zeros(drawing.size(), CvType.CV_8UC3);
-        List<MatOfPoint> countoursPolyListFinal =new ArrayList<>(countoursPolyFinal.length);
-        for(MatOfPoint2f poly : countoursPolyFinal){
+        List<MatOfPoint> countoursPolyListFinal = new ArrayList<>(countoursPolyFinal.length);
+        for (MatOfPoint2f poly : countoursPolyFinal) {
             countoursPolyListFinal.add(new MatOfPoint(poly.toArray()));
         }
-        for(int i=0;i<countoursFinal.size();i++){
-            Scalar color =new Scalar(255,0,0,0);
-           // Imgproc.drawContours(finalIm,countoursPolyListFinal,i,color);
-            Imgproc.rectangle(finalIm,boundRectFinal[i].tl(),boundRectFinal[i].br(),color,2);
-        }
-        List<Mat> detectobj =new ArrayList<>();
-        for(int i=0;i<boundRectFinal.length;i++){
-               Mat temp = new Mat(wall,boundRectFinal[i]);
-               temp=temp.clone();
-               detectobj.add(i,temp);
-        }
-        Mat t =detectobj.get(0);
-        Bitmap m =Bitmap.createBitmap(t.cols(),t.rows(),Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(t,m);
-        imgView.setImageBitmap(m);
-        DescriptorMatcher matcher=DescriptorMatcher.create(DescriptorMatcher.FLANNBASED);
-        Mat descriptors2=new Mat(),descriptors1=new Mat();
-        MatOfKeyPoint keypoints1=new MatOfKeyPoint();
-        MatOfKeyPoint keypoints2 =new MatOfKeyPoint();
-        ORB detecctor=ORB.create();
-        int[] arr= new int [12];
-        for(int j=0;j<12;j++){
-            detecctor.detectAndCompute(t, new Mat(), keypoints1, descriptors1);
-            detecctor.detectAndCompute(object.get(j), new Mat(), keypoints2, descriptors2);
-            List<MatOfDMatch> knnMatches = new ArrayList<>();
-            matcher.knnMatch(descriptors1, descriptors2, knnMatches, 2);
-            float ratioThresh = 0.7f;
-            List<DMatch> listOfGoodMatches = new ArrayList<>();
-            for (int k = 0; k < knnMatches.size(); k++) {
-                if (knnMatches.get(k).rows() > 1) {
-                    DMatch[] matches = knnMatches.get(k).toArray();
-                    if (matches[0].distance < ratioThresh * matches[1].distance) {
-                        listOfGoodMatches.add(matches[0]);
-                    }
-                }
+        List<Rect> finalRectangle =new ArrayList<>();
+        for (int i = 0; i < countoursFinal.size(); i++) {
+            Scalar color = new Scalar(255, 0, 0, 0);
+            // Imgproc.drawContours(finalIm,countoursPolyListFinal,i,color);
+            if(boundRectFinal[i].area()>2100) {
+                finalRectangle.add(boundRectFinal[i]);
+                Imgproc.rectangle(finalIm, boundRectFinal[i].tl(), boundRectFinal[i].br(), color, 2);
             }
         }
 
+        List<Mat> detectobj = new ArrayList<>();
+        for (int i = 0; i < finalRectangle.size(); i++) {
+            Mat temp = new Mat(wall, finalRectangle.get(i));
+            temp = temp.clone();
+            detectobj.add(i, temp);
+        }
+
+        for (int i = 0; i < detectobj.size(); i++) {
+            Mat temp = detectobj.get(i);
+            Imgproc.resize(temp, temp, new Size(1000, 1000));
+            detectobj.set(i, temp);
+        }
+
+        for (int i = 0; i < object.size(); i++) {
+            Mat temp = object.get(i);
+            Imgproc.resize(temp, temp, new Size(1000, 1000));
+            object.set(i, temp);
+        }
+
+        Mat ma=detectobj.get(2);
+        double min=888888888;
+        int minIndex=-1;
+        for(int i=0;i<object.size();i++){
+            Mat temp= object.get(i);
+            Mat diff =new Mat();
+            Core.subtract(temp,ma,diff);
+            diff.mul(diff);
+            Scalar s= Core.sumElems(diff);
+            double pa =s.val[0];
+            if(pa<min){
+                min=pa;
+                minIndex=i;
+            }
+        }
+        Toast.makeText(this,"Index"+ minIndex,Toast.LENGTH_SHORT).show();
 
 
 
 
 
+        }
 
-
-    }
 
 
 
